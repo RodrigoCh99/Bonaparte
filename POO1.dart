793504e15f52a4da2ed96cee3cca389b0d99c4f5
Rodrigo Pereira da Rocha <37093671+RodrigoCh99@users.noreply.github.com>
2022-04-06 10:53:17 -0300

    atualização POO

--- POO1.dart
+++ POO1.dart
@@ -38,6 +38,12 @@ void main() {
   pessoa2.trocarNome('Gustavo');
   
   print('DADOS SOBRE A PESSOA2: \nNOME:  ${pessoa2.nome}\nIDADE: ${pessoa2.idade}\nCASADO: ${pessoa2.casado}');
+  
+  NovaPessoa np1 = NovaPessoa(nome: 'Emily', idade: 20);
+  np1.dinheiro = 300;
+  np1.dinheiro = 2;
+  np1.dinheiro = 10000000000000000000;
+  print(np1.dinheiro);
 }
 
 // CRIAÇÃO DE UMA CLASSE 
@@ -80,5 +86,67 @@ class Pessoa {
     nome = n;
   }
   
+}
+
+// CRIAÇÃO DE UMA CLASSE COM CONSTRUTOR
+class NovaPessoa {
+  
+  NovaPessoa({required String this.nome, required int this.idade, this.casado = false}) {
+    print('\n\n--------------------------------');
+    print('Criando $nome com idade $idade');
+    print('--------------------------------');
+  }
+  
+  String nome;
+  int idade;
+  bool casado = false;
+  
+  // atributo privado não deve ser acessado diretamente
+  double? _dinheiro;
+  
+  // MÉTODOS DA CLASSE PESSOA:
+  void aniversario() {
+    print('\n===============================');
+    print('parabéns! ${nome}');
+    print('===============================\n');
+    
+    idade = idade++;
+   
+  }
+  
+  void casar() {
+    
+    print('\n-----------------------------');
+    print('triste! ${nome}');
+    print('-------------------------------\n');
+    
+    casado = true;
+  }
+  
+  void trocarNome(String n) {
+    
+    print('\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+    print('trocou de nome!');
+    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n');
+    
+    nome = n;
+    
+  }
+  
+  // criando um método set
+  set dinheiro(double? valor) {
+    if (valor != null && valor >= 0 && valor < 1000000) {
+      print('modificando o dinheiro de $nome');
+      _dinheiro = valor; 
+    } else {
+      print('não modificou o dinheiro de $nome');
+    }
+  }
+  
+  // crinado um método get
+  double? get dinheiro {
+    print('Lendo dinheiro de $nome');
+    return _dinheiro;
+  }
   
 }
