e9dbf0b0f5a3dffe812d7e63f619fa41de19d119
Rodrigo Pereira da Rocha <37093671+RodrigoCh99@users.noreply.github.com>
2022-04-06 13:11:15 -0300

    Update POO2.dart

--- POO2.dart
+++ POO2.dart
@@ -1,1 +1,65 @@
+// HERANÇA
+void main () {
+  Cachorro rex = Cachorro();
+  rex.nome = 'rex';
+  rex.idade = 2;
+  
+  rex.dormir();
+  rex.comer();
+  rex.latir();
+  
+  Gato tom = Gato();
+  tom.nome = 'tom';
+  tom.idade = 3;
+  
+  tom.dormir();
+  tom.comer();
+  tom.miar();
+  
+  List<Animal> animais = [];
+  
+  animais.add(rex);
+  animais.add(tom);
+  
+  print(animais.first);
+    
+}
 
+abstract class Animal {
+  String? nome;
+  int? idade;
+  
+  void comer() {
+    print('comeu');
+  }
+  
+  void dormir() {
+    print('dormiu');
+  }
+  
+  void morrer();
+}
+
+class Cachorro extends Animal {
+  
+  void latir() {
+    print('AU AU');
+  }
+  
+  void morrer() {
+    print('dog deeath');
+  }
+   
+}
+
+class Gato extends Animal {
+  
+  void miar() {
+    print('MIAUUUU');
+  }
+  
+  void morrer() {
+    print('cat deeath');
+  }
+  
+}
